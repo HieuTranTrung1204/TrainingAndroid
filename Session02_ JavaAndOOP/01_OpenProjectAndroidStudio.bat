27d3973ba306775b8afbb47be08f84c525304a23
hieu.trantrung1204@icloud.com <z@?123456>
2017-11-13 02:15:46 +0700

    * fix script open AndroidStudio project

--- Session02_ JavaAndOOP/01_OpenProjectAndroidStudio.bat
+++ Session02_ JavaAndOOP/01_OpenProjectAndroidStudio.bat
@@ -1,3 +1,4 @@
-set PATH_ANDROID_STUDIO="C:\Program Files\Android\Android Studio"
-set PATH_PROJECT="C:\Users\TrungHieuTran\AndroidStudioProjects\HelloJava"
-call %PATH_ANDROID_STUDIO%\bin\studio64.exe %PATH_PROJECT%
+set PATH_ANDROID_STUDIO="C:\Program Files\Android\Android Studio\bin\studio64.exe"
+set PATH_PROJECT="D:\Training\ITTCHUFI\AndroidTraining\Session02_ JavaAndOOP\HelloJava"
+cls
+call %PATH_ANDROID_STUDIO% %PATH_PROJECT%
